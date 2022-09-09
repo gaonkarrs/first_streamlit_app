efd5957b511f95bed79bc741cc98e9ee0ff6b843
Rajendra Gaonkar <33920764+gaonkarrs@users.noreply.github.com>
2022-09-09 23:57:14 +0530

    Update streamlit_app.py
    Fruit list from S3 file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Favorites")
@@ -8,3 +9,6 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruits_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
